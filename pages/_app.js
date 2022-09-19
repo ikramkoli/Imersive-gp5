a80efa9f9cd217a1eacb22d3d09f5c82fc7bdd4a
ikram abdillah <ikram.koli@gmail.com>
2022-09-19 14:02:12 +0700

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
